1c6f71220778f7011ed598ca00ccb5e0e953ab25
Phan Ha <phanthanhhak55hus@gmail.com>
2016-03-14 22:03:26 +0700

    site ba

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -1,23 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("#owl-slider").owlCarousel({
         navigation: true,
-        navigationText: false,
+        navigationText: true,
         slideSpeed: 300,
         paginationSpeed: 400,
-        singleItem: true
+        singleItem: true,
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        navigation: true,
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [979, 3],
+        itemsTablet: [768, 2],
+        dots :true,
+        lazyLoad:true
+        
     });
     $(".owl-product").owlCarousel({
         autoPlay: 3000,
         navigation: true,
-        navigationText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-        items : 4,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3],
-        itemsTablet: [768,2]
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        items: 4,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3],
+        itemsTablet: [768, 2]
     });
     $("#owl-client").owlCarousel({
         navigation: true,
-        navigationText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
         slideSpeed: 300,
         paginationSpeed: 400,
         singleItem: true
@@ -26,66 +34,66 @@ $(document).ready(function(){
         autoPlay: 3000,
         navigation: true,
         navigationText: false,
-        items : 6,
-        itemsDesktop : [1199,4],
-        itemsDesktopSmall : [979,3],
-        itemsTablet : [768,2]
+        items: 6,
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [979, 3],
+        itemsTablet: [768, 2]
     });
     $("#owl-product").owlCarousel({
         autoPlay: 3000,
         navigation: true,
         navigationText: false,
-        items : 4,
-        itemsDesktop : [1199,4],
-        itemsDesktopSmall : [979,4],
-        itemsTablet : [768,4],
-		itemsMobile : [479,3],
+        items: 4,
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [979, 4],
+        itemsTablet: [768, 4],
+        itemsMobile: [479, 3],
     });
-	$(".owl-product-sale").owlCarousel({
+    $(".owl-product-sale").owlCarousel({
         autoPlay: 3000,
         navigation: true,
-        navigationText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-        items : 1,
-        itemsDesktop : [1199,1],
-        itemsDesktopSmall : [979,1],
-        itemsTablet : [768,2],
-		itemsMobile : [479,1]
-	});
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        items: 1,
+        itemsDesktop: [1199, 1],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 2],
+        itemsMobile: [479, 1]
+    });
     $("#nav-mobile").mmenu();
-    $(".nav-category ul li i").on("click", function(){
+    $(".nav-category ul li i").on("click", function () {
         $(this).parent().children("ul").toggle();
     });
-    $(".facq-up").on("click", function(){
+    $(".facq-up").on("click", function () {
         var quantity = parseInt($(".quantity").val());
-        if(quantity < 1000) {
+        if (quantity < 1000) {
             quantity = quantity + 1;
             $(".quantity").val(quantity);
         }
     });
-    $(".facq-down").on("click", function(){
+    $(".facq-down").on("click", function () {
         var quantity = parseInt($(".quantity").val());
-        if(quantity > 0) {
+        if (quantity > 0) {
             quantity = quantity - 1;
             $(".quantity").val(quantity);
         }
     });
-    $(".cct-grid").on("click", function(){
+    $(".cct-grid").on("click", function () {
         $(".cct-list").removeClass("active");
         $(this).addClass("active");
         $(".cc-products").removeClass("cc-list");
     });
-    $(".cct-list").on("click", function(){
+    $(".cct-list").on("click", function () {
         $(".cct-grid").removeClass("active");
         $(this).addClass("active");
         $(".cc-products").addClass("cc-list");
     });
-    jQuery(document).ready(function($){  
+    jQuery(document).ready(function ($) {
         $("#yt-totop").hide();
         $(function () {
             var wh = $(window).height();
-            var whtml =  $(document).height();
+            var whtml = $(document).height();
             $(window).scroll(function () {
-                if ($(this).scrollTop() > whtml/10) {
+                if ($(this).scrollTop() > whtml / 10) {
                     $('#yt-totop').fadeIn();
                 } else {
                     $('#yt-totop').fadeOut();
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,5 +1,5 @@
 @extends('frontend.master')
-@section('title', 'Công ty TNHH phân phối sách')
+@section('title', 'Gold season')
 @section('trangchu', 'active')
 @section('content')
 <!-- slide --- banner -->
@@ -8,9 +8,4 @@
 <!-- san pham mới -->
 @include('frontend.blocks.product_new')
 <!-- end sản phẩm mới -->
-<!-- san pham khuyeens mai -->
-@include('frontend.blocks.product_sale')
-<!-- end sản phẩm khuyến mại-->
-<!-- san pham noi bat -->
-@include('frontend.blocks.banchay')
 @stop
